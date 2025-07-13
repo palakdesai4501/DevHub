304a7025e0220df59571a273f42e191b75e8ef97
Palak Desai <palak.desai4501@gmail.com>
2025-07-13 13:43:07 -0400

    backend: Working on backend of DevHub

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.get('mongoURI'));
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('Database connection error:', err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB; 
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+// Define Routes
+app.get('/', (req, res) => res.send('API Running'));
+
+// Route handlers
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/posts', require('./routes/posts'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
